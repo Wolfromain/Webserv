adeefc233a3bf1d7e1f22ab84af3a0078eccc534
rbouquet <rbouquet@student.42.fr>
2025-07-04 13:14:48 +0200

    .conf

---
+++ include/CGI.hpp
@@ -0,0 +1,26 @@
+#ifndef CGI_HPP
+# define CGI_HPP
+
+# include <iostream>
+# include <string>
+# include <cstdlib>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <cstring>
+# include <sys/stat.h>
+
+class CGI
+{
+	private:
+
+	
+	public:
+
+		CGI();
+		~CGI();
+
+};
+
+#endif
